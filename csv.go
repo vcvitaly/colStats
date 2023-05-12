b2f96deef4e813332847de744ef8ab2cdd7fe8ec
Vitalii Chura <vcvitaly@yahoo.com>
2023-05-12 19:17:09 +0300

    read files line by line

--- csv.go
+++ csv.go
@@ -24,18 +24,22 @@ func avg(data []float64) float64 {
 func csv2Float(r io.Reader, column int) ([]float64, error) {
 	// Create the CSV Reader used to read in data from CSV files
 	cr := csv.NewReader(r)
+	cr.ReuseRecord = true
 	// Adjusting for 0 based index
 	column--
 
-	// Read in all CSV data
-	allData, err := cr.ReadAll()
-	if err != nil {
-		return nil, fmt.Errorf("Cannot read data from file: %w", err)
-	}
-
 	var data []float64
 	// Looping through all records
-	for i, row := range allData {
+	for i := 0; ; i++ {
+		row, err := cr.Read()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			return nil, fmt.Errorf("Cannot read data from file: %w", err)
+		}
+
 		if i == 0 {
 			continue
 		}
